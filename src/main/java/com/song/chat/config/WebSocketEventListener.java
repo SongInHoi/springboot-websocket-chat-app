b29cd1ca05c04da884cde7d4da242f95cd1f7b47
songinhoi <koreasong97@naver.com>
2025-09-15 17:42:32 +0900

    연결 해제 이벤트 리스너 완료

--- src/main/java/com/song/chat/config/WebSocketEventListener.java
+++ src/main/java/com/song/chat/config/WebSocketEventListener.java
@@ -1,18 +1,33 @@
 package com.song.chat.config;
 
+import com.song.chat.dto.ChatMessage;
+import com.song.chat.dto.MessageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class WebSocketEventListener {
 
+    final private SimpMessageSendingOperations messageTemplate;
+
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
-
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if(username != null) {
+            log.info("user disconnected: {}", username);
+            var chatMessage = ChatMessage.builder()
+                    .type(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+            messageTemplate.convertAndSend("/topic/public", chatMessage);
+        }
     }
 }
